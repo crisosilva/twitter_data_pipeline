e23a14709e97275d45f31dbbca7bd11646ca3f54
crisosilva <crisosilva88@gmail.com>
2022-03-29 17:57:52 -0300

    padronização no script

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 
-# [START Data_pipeline]
+
+# [START Twitter_data_pipeline]
+# [START import_modules]
 from datetime import datetime, timedelta
+
+from textwrap import dedent
 
 from airflow.models import DAG
 from airflow.utils.dates import days_ago
 from airflow.decorators import task
 from airflow.utils.task_group import TaskGroup
-from airflow.operators import python
+from airflow.operators.python import PythonOperator
+# [END import_modules]
 
 
-# [START Data_pipeline_define_dag]
-
+# [START defining_args]
 default_args = {
     'owner': 'Cristiano Silva',
     'start_date': days_ago(0),
@@ -20,17 +41,24 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
+#  [END defining_args]
 
+# [START instantiate_DAG]
 with DAG(
     'twitter_data_pipeline',
     schedule_interval=timedelta(minutes=5),
     default_args = default_args
     ) as dag:
-# [END Data_pipeline_define_dag]
+    # [END instantiate_DAG]
 
-    # [START Data_pipeline_operators]
-    def get_trends():
+    # [START documentation]
+    dag.doc_md = __doc__
+    # [END documentation]
+
+    # [START extract]
+    def extract(**kwargs):
         import tweepy
+        ti = kwargs['ti']
 
         # Keys for twitter API
         consumer_key = 'xxx'
@@ -47,48 +75,76 @@ with DAG(
         BRAZIL_WOE_ID = 23424768
 
         brazil_trends = api.get_place_trends(id=BRAZIL_WOE_ID)
-        return brazil_trends
+        ti.xcom_push('data_tts_brazil', brazil_trends)
+    # [END extract]
 
-    def transform_twitter_data(data) -> None:
+    # [START transform]
+    def transform(**kwargs):
+        ti = kwargs['ti']
 
         trends_list = []
+        data = ti.xcom_pull(task_ids='extract', key='data_tts_brazil')
         for value in data:
             for trend in value['trends']:
                 trends_list.append(trend['name'])
 
         trends_list.insert(0, datetime.now().strftime('%d/%m/%Y %H:%M'))
 
-        return trends_list[0:11]
+        ti.xcom_push('trends', trends_list[0:11])
+    # [END transform]
 
+    # [START publish_data]
+    def kafka_publish(**kwargs):
 
-
-    def kafka_publish(b_server, kafka_topic, message) -> None:
-
+        ti = kwargs['ti']
         from kafka import KafkaProducer
 
-        producer = KafkaProducer(bootstrap_servers=b_server, #,value_serializer=lambda x: dumps(x).encode('utf-8')
+        producer = KafkaProducer(bootstrap_servers='localhost:9090', #,value_serializer=lambda x: dumps(x).encode('utf-8')
                                  max_block_ms=60000,
                                  api_version=(0, 10, 2)
                                  )
-        data = {'twitter_trends': message}
+        data = {'twitter_trends': ti.xcom_pull(task_ids='transform', key='trends')}
         try:
             print("aqui")
-            producer.send(kafka_topic, value=message)
+            producer.send('tts_pipeline', value=ti.xcom_pull(task_ids='transform', key='trends'))
         except:
             print("Erro ao tentar enviar a mensagem")
+    # [END publish_data]
 
-    extract = python.PythonOperator(task_id='twitter_extract',
-                                    python_callable=get_trends())
+    # [START main_flow]
+    extract_task = PythonOperator(task_id='extract',
+                                  python_callable=extract)
+    extract_task.doc_md = dedent(
+        """\
+    #### Extract task
+    A simple Extract task to get data ready for the rest of the data pipeline.
+    In this case, getting data is simulated by reading from a hardcoded JSON string.
+    This data is then put into xcom, so that it can be processed by the next task.
+    """
+    )
 
-    transform = python.PythonOperator(task_id='transform_data',
-                                      python_callable=transform_twitter_data())
+    transform_task = PythonOperator(task_id='transform',
+                                    python_callable=transform)
+    transform_task.doc_md = dedent(
+        """\
+    #### Transform task
+    A simple Transform task which takes in the collection of order data from xcom
+    and computes the total order value.
+    This computed value is then put into xcom, so that it can be processed by the next task.
+    """
+    )
 
-    send = python.PythonOperator(task_id='kafka_send',
-                                 python_callable=kafka_publish(transform.python_callable('localhost:9092','tts_data_pipeline',extract.python_callable)))
-    # [END Data_pipeline_operators]
+    publish_task = PythonOperator(task_id='publish',
+                                  python_callable=kafka_publish)
+    publish_task.doc_md = dedent(
+        """\
+    #### Load task
+    A simple Load task which takes in the result of the Transform task, by reading it
+    from xcom and instead of saving it to end user review, just prints it out.
+    """
+    )
 
-# [ START Data_pipeline_relationships]
-extract >> transform >> send
-# [ END Data_pipeline_relationships]
+    extract_task >> transform_task >> publish_task
+    # [END main_flow]
 
-# [END Data_pipeline]
+# [END Twitter_data_pipeline]
